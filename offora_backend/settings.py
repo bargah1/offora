29930ff9f687b9fd815be9cee39ac02cab4041fa
sabithh <mxsabith17@gmail.com>
2025-07-26 13:49:07 +0530

    update in settings for razopay

--- offora_backend/settings.py
+++ offora_backend/settings.py
@@ -145,8 +145,8 @@ REST_FRAMEWORK = {
 }
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-RAZORPAY_KEY_ID = 'rzp_test_30B5GDfc4NrU1X'
-RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRE')
+RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID')
+RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
